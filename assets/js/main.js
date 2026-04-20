0114a620e6c1d3be90d121d04abb73e94cdac818
felip-araujo <felipearaujoexecutive@gmail.com>
2026-04-20 16:20:34 -0400

    mail2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,26 @@
-alert("teste")
+console.log("teste")
 
-async function enviarFormulario(nome, email, mensagem) {
+const form = document.getElementById("form-contato");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault(); // impede reload
+
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const mensagem = document.getElementById("mensagem").value;
+
   const res = await fetch("/api/contato", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({
-      nome,
-      email,
-      mensagem
-    })
+    body: JSON.stringify({ nome, email, mensagem })
   });
 
   if (res.ok) {
-    alert("Enviado com sucesso!");
+    alert("Mensagem enviada!");
+    form.reset();
   } else {
     alert("Erro ao enviar");
   }
-}
+});
